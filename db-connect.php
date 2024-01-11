828dda8587dc9a3538cc5d299ce6ab0f20dffdaa
sachi1048 <148302451+sachi1048@users.noreply.github.com>
2024-01-11 11:35:00 +0900

    i

---
+++ db-connect.php
@@ -0,0 +1,8 @@
+<?php
+     const SERVER = 'mysql214.phy.lolipop.lan';
+     const DBNAME = 'LAA1517472-final';
+     const USER = 'LAA1517472';
+     const PASS = 'Pass1026';
+
+    $connect = 'mysql:host='. SERVER . ';dbname='. DBNAME . ';charset=utf8';
+?>
---
+++ menu.php
@@ -0,0 +1,5 @@
+<a href="list.php">一覧</a>
+<a href="author.php">作者別</a>
+<a href="update.php">更新</a>
+<a href="delete.php">削除</a>
+<hr>
